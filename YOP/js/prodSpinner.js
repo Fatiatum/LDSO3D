e65d649719b3ee60f69f69a476d67de9efa387f7
mariamiranda <maria.miranda725@gmail.com>
2015-11-26 03:03:35 +0000

    "Product page updated"

--- YOP/js/prodSpinner.js
+++ YOP/js/prodSpinner.js
@@ -4,3 +4,15 @@ $('.spinner').spinner({
   min: 0,
   max: 10
 });
+
+//Date
+$(document).ready(function() {
+    $('#datePicker')
+        .datepicker({
+            format: 'mm/dd/yyyy'
+        })
+        .on('changeDate', function(e) {
+            // Revalidate the date field
+            $('#eventForm').formValidation('revalidateField', 'date');
+        });
+
