ea5d6ab2f055d066c783e2f5e1690375f94fa90c
NunoGhost <up201502854@fe.up.pt>
2016-01-03 21:05:30 +0000

    more functional tests

--- Rails/test/controllers/faq_controller_test.rb
+++ Rails/test/controllers/faq_controller_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class FaqControllerTest < ActionController::TestCase
   
+  setup do
+    @faq = faqs(:one)
+  end
+
   test "should get index with locale en" do
     get :index, :locale => :en
     assert_response :success
@@ -13,5 +17,16 @@ class FaqControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil(:faqs)
   end
+
+  test "should get edit with locale en" do
+    get :edit, id: @faq, :locale => :en
+    assert_response :success
+  end
+
+  test "should get edit with locale pt" do
+    get :edit, id: @faq, :locale => :pt
+    assert_response :success
+  end
+
 
 end
