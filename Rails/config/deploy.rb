a11b73c159d65ee71b16e77f07854e79aa7da3fb
Jorge Teixeira <Jorge Teixeira>
2016-10-29 23:34:26 +0200

    deploy filed changed

--- Rails/config/deploy.rb
+++ Rails/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.4.0'
+lock '3.1.0'
 
 set :application, 'YOP'
 set :repo_url, 'git@github.com:Fatiatum/LDSO3D.git'
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:Fatiatum/LDSO3D.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/home/deploy/YOP'
+set :deploy_to, '/home/deploy/YOP'
 
 # Default value for :scm is :git
 # set :scm, :git
